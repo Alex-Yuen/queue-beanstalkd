87016c4857466d68da235107dcb19ea19be7e268
AnQin <an-qin@qq.com>
2020-03-14 23:28:31 +0800

    修正删除任务失败

--- src/queue/job/Beanstalkd.php
+++ src/queue/job/Beanstalkd.php
@@ -13,6 +13,7 @@ use an\queue\connector\Beanstalkd as Banstalkd;
 use think\{queue\Job, App};
 
 class Beanstalkd extends Job {
+    private ?Banstalkd $banstalkd = null;
     private ?\Pheanstalk\Job $job = null;
     private int $attempts = 1;
     private array $parseRaw = [];
@@ -22,13 +23,13 @@ class Beanstalkd extends Job {
         $this->app = $app;
         $this->job = $job;
         /** @var Banstalkd instance */
-        $this->instance = $beanstalk;
+        $this->banstalkd = $beanstalk;
         $this->connection = $connection;
         $this->attempts = $this->payload()['attempts'] ?? 1;
     }
 
     public function payload(): array {
-        return empty($this->parseRaw) ? $this->parseRaw = $this->instance->unpack($this->job->getData()) : $this->parseRaw;
+        return empty($this->parseRaw) ? $this->parseRaw = $this->banstalkd->unpack($this->job->getData()) : $this->parseRaw;
     }
 
     public function getJobId(): int {
@@ -42,16 +43,15 @@ class Beanstalkd extends Job {
     public function release($delay = 0) {
         parent::release($delay);
         $this->delete();
-        $this->instance->release($this->queue, $this->payload(), $delay, ++$this->attempts);
+        $this->banstalkd->release($this->queue, $this->payload(), $delay, ++$this->attempts);
     }
 
     public function delete() {
         parent::delete();
-        $this->instance->deleteReserved($this->job);
+        $this->banstalkd->deleteReserved($this->job);
     }
 
     public function getRawBody(): ?array {
-        dump($this->job->getData());
         return $this->payload();
     }
 }
